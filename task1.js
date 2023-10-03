9363071810fc2a9fc4d3a120ab14b55c1f205cbe
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-03 17:32:56 +0300

    Create the check and if this checks successful then it logs the key and value to console

--- task1.js
+++ task1.js
@@ -1,6 +1,8 @@
 // Write a function that takes any object and logs every key and value from only own properties.
 function logOwnValues(obj) {
 	for (let key in obj) {
-		
+		if (obj.hasOwnProperty(key)) {
+			console.log(key, obj[key]);
+		}
 	}
 }
