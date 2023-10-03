ae969aa10ab06947e101ea10c8c46237fe3f7c67
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-03 17:39:12 +0300

    Write the code of the isInObject function

--- task2.js
+++ task2.js
@@ -1,5 +1,5 @@
 // Write a function that takes string and object as arguments then checks if the object has the key wih value of string.
 
 function isInObject(key, obj) {
-	
+	return key in obj;
 }
