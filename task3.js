f54f544d09fefc681649827a6e53cfd147bdbcdc
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-03 17:49:38 +0300

    Change returnObjWithoutPrototype to createObjWithoutPrototype

--- task3.js
+++ task3.js
@@ -1,13 +1,13 @@
 // Write a function that creates empty object but without the prototype.
 
-function returnObjWithoutPrototype() {
+function createObjWithoutPrototype() {
 	obj = new Object();
 	obj.__proto__ = null;
 	return obj;
 }
 
 // Tests
-const newObj = returnObjWithoutPrototype();
+const newObj = createObjWithoutPrototype();
 console.log(newObj); // {}
 console.log(newObj.__proto__); // undefined
 console.log(Object.getPrototypeOf(newObj)); // null
